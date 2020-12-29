172fc013f7dee14a0868abaedca0c41a6f593d04
Gita Grīva <gita.griva@valmiera.edu.lv>
2020-12-29 09:49:09 +0000

    Uzdevumi izveidoti

--- serveris.py
+++ serveris.py
@@ -12,6 +12,22 @@ def index():
 def sveiki():
   return "Nav vairs nekāds rīts!"
 
+@app.route('/sveiki/<vards>')
+def sveikiPersona(vards):
+  return f"Sveiki {vards} !"  #'sveiki{}'.format(vards)
+
+
+@app.route('/cik2/<reizinamais>')
+def cik2Reizinajums(reizinamais):
+  reizinamais = int(reizinamais)
+  print(type(reizinamais))
+  return str(reizinamais * 2)
+
+@app.route('/cik/<sk1>/<sk2>')
+def cik2Summa(sk1,sk2):
+  sk1 = int(sk1)
+  sk2 = int(sk2)
+  return str(sk1 + sk2)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
